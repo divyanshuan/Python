7e1bf23e0162427190a5b4c5b1be1c33e166dc1b
divyanshu verma (202081) <89985317+divyanshuan@users.noreply.github.com>
2022-08-17 23:57:43 +0530

    day7

--- day7.py
+++ day7.py
@@ -1,1 +1,21 @@
 # just fake commit 
+#list in python  
+mylist=["apple","banana","orange"]
+print(mylist)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(len(mylist))
+
+mylist.insert(2,"cherry") 
+print(mylist)
+mylist.append("papaya")
+print(mylist)
+
+if "apple" in mylist:
+    print("apple is in the list")
+
+mylist2=["zebra","cow","elephant"]
+mylist.extend(mylist2)
+# mylist.extend(mylist)
+print(mylist)
